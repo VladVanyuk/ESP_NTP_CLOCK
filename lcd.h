3c1e4d45c14530cd01964460b4c5d5ceaf0ca3cf
Polina Cherkasova <polina_cherkasova@outlook.com>
2025-05-07 19:31:02 +0300

    fixed bigNum appearance (droplet bug still present)

--- lcd.h
+++ lcd.h
@@ -18,179 +18,176 @@ uint32_t lastPrintedClockTime = 0;
 
 
 void printTimeLCD(tm newTime, bool forcePrint = false)
-{
-    if (((newTime.tm_sec != lastPrintedTime.tm_sec) && (millis() - lastPrintedClockTime > 1000)) || forcePrint == true)
+{ 
+  if (((newTime.tm_sec != lastPrintedTime.tm_sec) && (millis() - lastPrintedClockTime > 1000)) || forcePrint == true)
+  {
+
+     if ((newTime.tm_min != lastPrintedTime.tm_min) || (newTime.tm_hour != lastPrintedTime.tm_hour ) )
+     {
+        LCD.clear();
+     }
+    
+    LCD.setCursor(0, 0);
+    LCD.print(newTime.tm_mday);
+    LCD.print('/');
+    if ((newTime.tm_mon + 1) < 10 )
     {
-        if ((newTime.tm_min != lastPrintedTime.tm_min) || (newTime.tm_hour != lastPrintedTime.tm_hour))
-        {
-            LCD.clear();
-        }
-
-        LCD.setCursor(0, 0);
-        LCD.print(newTime.tm_mday);
-        LCD.print('/');
-        if ((newTime.tm_mon + 1) < 10)
-        {
-            LCD.print('0');
-        }
-        LCD.print((newTime.tm_mon + 1));
-        LCD.print('/');
-        LCD.print(newTime.tm_year - 100);
-
-        LCD.setCursor(0, 1);
-        if (newTime.tm_hour < 10)
-        {
-            LCD.print('0');
-        }
-        LCD.print(newTime.tm_hour);
-
-        LCD.print(':');
-        if (newTime.tm_min < 10)
-        {
-            LCD.print('0');
-        }
-        LCD.print(newTime.tm_min);
-
-        LCD.print(':');
-        if (newTime.tm_sec < 10)
-        {
-            LCD.print('0');
-        }
-        LCD.print(newTime.tm_sec);
-        lastPrintedTime = newTime;
+       LCD.print('0');
     }
+    LCD.print((newTime.tm_mon + 1));
+    LCD.print('/');
+    LCD.print(newTime.tm_year - 100);
+
+    LCD.setCursor(0, 1);
+    if(newTime.tm_hour < 10)
+    {
+      LCD.print('0');
+    }
+    LCD.print(newTime.tm_hour);
+
+    LCD.print(':');
+    if(newTime.tm_min < 10)
+    {
+      LCD.print('0');
+    }
+    LCD.print(newTime.tm_min);
+
+    LCD.print(':');
+    if(newTime.tm_sec < 10)
+    {
+      LCD.print('0');
+    }
+    LCD.print(newTime.tm_sec);
+    lastPrintedTime = newTime;
+  }
 }
 
 void setupChars()
 {
-    LCD.createChar(LCD_PACMAN_CL, pacmanClosed);
-    LCD.createChar(LCD_PACMAN, pacmanOpen);
-    LCD.createChar(LCD_DOT, dot);
-    LCD.createChar(LCD_TERMOMETER, termometer);
-    LCD.createChar(LCD_DROPLET, droplet);
+  LCD.createChar(LCD_PACMAN_CL, pacmanClosed);
+  LCD.createChar(LCD_PACMAN, pacmanOpen);
+  LCD.createChar(LCD_DOT, dot);
+  LCD.createChar(LCD_TERMOMETER, termometer);
+  LCD.createChar(LCD_DROPLET, droplet);
+  bigNum.begin();
 }
 
 uint8_t dropletPercent(float humidity)
 {
-    if (humidity < 15) //0-15
-    {
-        LCD.createChar(LCD_DROPLET, droplet);
-        return 0;
-    }
-    if (humidity < 30) //10-30
-    {
-        LCD.createChar(LCD_DROPLET, droplet_1);
-        return 1;
-    } else if (humidity < 50) //25-50
-    {
-        LCD.createChar(LCD_DROPLET, droplet_2);
-        return 2;
-    } else if (humidity < 65) //50 -65
-    {
-        LCD.createChar(LCD_DROPLET, droplet_3);
-        return 3;
-    } else if (humidity < 80) //65-80
-    {
-        LCD.createChar(LCD_DROPLET, droplet_4);
-        return 4;
-    } else //80-100
-    {
-        LCD.createChar(LCD_DROPLET, droplet_full);
-        return 5;
-    }
+  if (humidity < 15) //0-15
+  {
+     LCD.createChar(LCD_DROPLET, droplet);
+    return 0;
+  }
+  if (humidity < 30) //10-30
+  {
+     LCD.createChar(LCD_DROPLET, droplet_1);
+    return 1;
+  }
+  else if (humidity < 50) //25-50
+  {
+     LCD.createChar(LCD_DROPLET, droplet_2);
+    return 2;
+  }
+  else if (humidity < 65) //50 -65
+  {
+     LCD.createChar(LCD_DROPLET, droplet_3);
+    return 3;
+  }
+  else if (humidity < 80) //65-80
+  {
+     LCD.createChar(LCD_DROPLET, droplet_4);
+    return 4;
+  }
+  else //80-100
+  {
+     LCD.createChar(LCD_DROPLET, droplet_full);
+    return 5;
+  }
 }
 
 void printWeatherLCD(bool forcePrint = false, bool setChars = false)
 {
-    /*
-    if ((millis() - lastPrintedWeatherTime > timerSyncDelay_w) || forcePrint == true)
+  if ((millis() - lastPrintedWeatherTime > timerSyncDelay_w) || forcePrint == true)
+  {
+    if (setChars)
     {
-      if (setChars)
-      {
-        setupChars();
-        dropletPercent(Weather.humidity);
-      }
-
-      LCD.setCursor(9, 0);
-      LCD.write((char)LCD_TERMOMETER);
-      LCD.print((float)Weather.temp, 1);
-      LCD.print((char)223);   // lcd.write((char)0b11011111);
-      LCD.print('C');
-      //
-      LCD.setCursor(12, 1);//todo add % for droplets
-      LCD.write((char)LCD_DROPLET);
-      LCD.print(Weather.humidity);
-      LCD.print('%');
-      */
-
-    if ((millis() - lastPrintedWeatherTime > timerSyncDelay_w) || forcePrint)
-    {
-        lastPrintedWeatherTime = millis();
-        LCD.clear();
-
-        int tempInt = (int)Weather.temp;
-        float tempDecimal = Weather.temp - tempInt;
-
-        bigNum.displayLargeInt(tempInt, 0, 0, 2, false);
-        LCD.setCursor(10, 0);
-        LCD.print((char)223);
-        LCD.print('C');
-        LCD.setCursor(0, 1);
-        LCD.print("T:");
-        LCD.print(Weather.temp, 1);
-        LCD.write((char)LCD_TERMOMETER);
-
-        delay(5000);
-        LCD.clear();
-
-        int humInt = (int)Weather.humidity;
-        bigNum.displayLargeInt(humInt, 0, 0, 2, false);
-        LCD.setCursor(10, 0);
-        LCD.print('%');
-        LCD.setCursor(0, 1);
-        LCD.print("H:");
-        LCD.print(Weather.humidity, 0);
-        LCD.write((char)LCD_DROPLET);
-
-        delay(5000);
-        LCD.clear();
+      setupChars();
+      dropletPercent(Weather.humidity);
     }
+    LCD.clear();
+
+    LCD.setCursor(0, 1);
+    LCD.write((char)LCD_TERMOMETER);
+
+    int temp_dec = (int)((Weather.temp - (int)Weather.temp) * 10);
+    int temp_int = (int)Weather.temp;
+    bigNum.displayLargeInt(temp_int, 1, 0, 2, false);
+    if (temp_dec) {
+      bigNum.displayLargeInt(temp_dec, 9, 0, 1, false);
+      LCD.setCursor(8, 1);
+      LCD.print('.');
+      LCD.setCursor(12, 1);
+      LCD.print((char)223);
+      LCD.print('C');
+    } else {
+      LCD.setCursor(6, 1);
+      LCD.print((char)223);
+      LCD.print('C');
+    }
+
+    delay(10000);
+    LCD.clear();
+
+    int hum = (int)Weather.humidity;
+    bigNum.displayLargeInt(hum, 4, 0, 2, false);
+
+    LCD.setCursor(3, 1);
+    LCD.write((char)LCD_DROPLET);
+    LCD.setCursor(11, 0);
+    LCD.print('%');
+
+    delay(10000);
+    LCD.clear();
+  }
 }
+
+
 
 void lcdSetupOne()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
-    LCD.init();
-    LCD.backlight();
-    bigNum.begin();
-    setupChars();
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  LCD.init();
+  LCD.backlight();
+  bigNum.begin();
+  setupChars();
 
-    LCD.setCursor(0, 0);
-    LCD.print("WIFI Connecting");
+  LCD.setCursor(0, 0);
+  LCD.print("WIFI Connecting");
 }
 
 void lcdSetupTwo()
 {
-    LCD.clear();
-    LCD.setCursor(0, 0);
-    LCD.print("Setup done");
-    LCD.setCursor(0, 1);
-    LCD.print("Starting");
-    delay(3000);
-    LCD.clear();
-    LCD.setCursor(0, 0);
-    lastPrintedClockTime = millis();
-    printTimeLCD(timeinfo, true);
-    lastPrintedWeatherTime = millis();
-    printWeatherLCD(true, true);
-    digitalWrite(LED_BUILTIN, HIGH);
+  LCD.clear();
+  LCD.setCursor(0, 0);
+  LCD.print("Setup done");
+  LCD.setCursor(0, 1);
+  LCD.print("Starting");
+  delay(3000);
+  LCD.clear();
+  LCD.setCursor(0, 0);
+  lastPrintedClockTime = millis();
+  printTimeLCD(timeinfo, true);
+  lastPrintedWeatherTime = millis();
+  printWeatherLCD(true, true);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void lcdLoop()
 {
-    printTimeLCD(timeinfo);
-    printWeatherLCD();
+  printTimeLCD(timeinfo);
+  printWeatherLCD();
 }
 
 #endif
